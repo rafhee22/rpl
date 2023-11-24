c1091d7277b17bdf1513785a49bb1591d2d87242
muhammad Abdul Raafi <150180395+rafhee22@users.noreply.github.com>
2023-11-24 12:53:02 +0700

    Add files via upload

---
+++ fp/scip.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#titik3');
+const nav = document.querySelector('#menuaktif');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('hidden');
+})
+
+// const menu = document.querySelector('#titik3');
+const close = document.querySelector('#close');
+
+nav.addEventListener('click', function() {
+    nav.classList.toggle('hidden');
+})
+
